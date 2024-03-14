d91e6e3cb51765dae50e6b7e72f5e3ec56427293
diazale16 <turuter83@gmail.com>
2024-03-14 16:48:37 -0300

    avance con los ejercicios del 1a

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,7 +1,55 @@
-const App = () => {
+const Header = (props) => {
+  return (
+    <h1>
+      {props.course}
+    </h1>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part} {props.exercise}
+    </p>
+  )
+}
+
+const Content = (props) => {
   return (
     <div>
-      <p>Hello World</p>
+      <Part part={props.part1} exercise={props.exercise1}></Part>
+      <Part part={props.part2} exercise={props.exercise2}></Part>
+      <Part part={props.part3} exercise={props.exercise3}></Part>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <p>
+      Number of exercises {props.arg1 + props.arg2 + props.arg3}
+    </p>
+  )
+}
+
+const App = () => {
+  const course = "Half Stack application course"
+  const part1 = "Fundamentals of React"
+  const exercises1 = 10
+  const part2 = "Using props to pass data"
+  const exercises2 = 7
+  const part3 = "State of a component"
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course}></Header>
+      <Content
+        part1={part1} exercise1={exercises1}
+        part2={part2} exercise2={exercises2}
+        part3={part3} exercise3={exercises3}
+      ></Content>
+      <Total arg1={exercises1} arg2={exercises2} arg3={exercises3}></Total>
     </div>
   )
 }
