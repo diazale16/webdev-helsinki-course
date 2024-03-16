c7972899be9ec4acebe7190897198b9d434e5fc1
diazale16 <turuter83@gmail.com>
2024-03-15 21:50:10 -0300

    done exercise 1.7 in part 1d

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -21,32 +21,35 @@ const App = () => {
   const [ good, setGood] = useState(0)
   const [ neutral, setNeutral] = useState(0)
   const [ bad, setBad] = useState(0)
+  const [ allOpinions, setAll] = useState(0)
 
-  // console.log(`rendering with counter value equals ${counter}`)
-
-  // Increase the counter for every click
   const handlerGoodFeedback = () => {
-    // console.log(`increasing value from the original ${counter}`)
     const newGood = good + 1
     setGood(newGood)
+    setAll(newGood + neutral + bad)
   }
 
   const handlerNeutralFeedback = () => {
-    // console.log(`decreasing value from the original ${counter}`)
     const newNeutral = neutral + 1
     setNeutral(newNeutral)
+    setAll(good + newNeutral + bad)
   }
 
   const handlerBadFeedback = () => {
     const newBad = bad + 1
     setBad(newBad)
+    setAll(good + neutral + newBad)
   }
 
   const handlerReset = () => {
     setGood(0)
     setNeutral(0)
     setBad(0)
+    setAll(0)
   }
+
+  const averageScore = (good - bad)/(good + neutral + bad)
+  const positivePercent = (good / allOpinions * 100)
 
   return (
     <div>
@@ -58,6 +61,9 @@ const App = () => {
       <Display text={"good"} counter={good}></Display>
       <Display text={"neutral"} counter={neutral}></Display>
       <Display text={"bad"} counter={bad}></Display>
+      <Display text={"all"} counter={allOpinions}></Display>
+      <Display text={"average"} counter={averageScore}></Display>
+      <Display text={"positive"} counter={positivePercent}></Display>
       <Button text={"reset"} handlerClick={handlerReset}></Button>
     </div>
   )
